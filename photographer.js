18f96a97e4732b601f16629dff1b1fcefd20049c
Micah Elizabeth Scott <micah@misc.name>
2015-01-21 22:32:28 -0800

    Rearchitecture, with dark frames and JSON storage

--- photographer.js
+++ photographer.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 /*
- * Experimental tool for gathering high quality RAW photographs
- * of each LED in an installation, for later processing into layouts
- * and image-based models of various sorts.
+ * Data gathering tool:
+ *
+ *   - Detects the attached Fadecandy boards
+ *   - Detects the length of attached LED strips
+ *   - Takes high quality RAW photographs of each active LED
+ *   - Generates tiny grayscale thumbnails
+ *   - Writes results to "photos.json"
+ *
+ * If you need to restart data gathering, this tool will avoid
+ * retaking any photos that it's already taken, but existing
+ * photos will be reprocessed.
  *
  * Copyright (c) 2015 Micah Elizabeth Scott
  * Released under the MIT license, see the accompanying LICENSE file.
@@ -11,167 +19,300 @@
 var fadecandy = require('./lib/fadecandy.js');
 var GPhoto = require('gphoto2');
 var async = require('async');
-var sprintf = require('sprintf-js').sprintf;
 var fs = require('fs');
+var path = require('path');
 var util = require('util');
-var crypto = require('crypto');
+var randy = require('randy');
 var cv = require('opencv');
 var exec = require('child_process').exec;
 
+var THUMBNAIL_SCALE_LOG2 = 3;   // Log2 of the amount to downscale thumbnails by before storage
+var NOISE_THRESHOLD = 10;       // Diffs less than this mean the image hasn't changed meaningfully
+var MAXIMUM_GAP = 2;            // Maximum number of consecutive missing LEDs before we give up on a strip
+var DARK_INTERVAL = 60;         // Take a new dark frame every N seconds
 
-var peakImageDiff = function (img1, img2)
+
+var tempCounter = 0;
+
+function tempSuffix()
 {
-    /*
-     * Compare two images, calculating the peak absolute value difference between them.
-     * This is better than mean squared error for our purposes, since we expect there to
-     * be localized hotspots that differ with most of the image remaining the same.
-     */
+    tempCounter += 1;
+    return '_tmp' + tempCounter;
+}
 
-    var diff = new cv.Matrix(img1.width(), img1.height());
-    diff.absDiff(img1, img2);
-    return diff.minMaxLoc().maxVal;
+function atomicWriteFile(filename, data, callback)
+{
+    var tempFile = filename + tempSuffix();
+    fs.writeFile(tempFile, data, function (err) {
+        if (err) return callback(err);
+        fs.rename(tempFile, filename, callback);
+    });
 }
 
 
-var LEDStrip = function (device, index)
+function generateThumbnail(name, io, jNode, callback)
 {
     /*
-     * One LED strip has LEDs that are likely to be physically nearby.
-     * We pseudorandomly choose remaining LEDStrips to photograph.
-     * Each time we photograph an LEDStrip, we light up the next LED in sequence.
-     * If we see a single missing LED, we'll skip over it
-     * If we see several missing LEDs, we assume the strip has ended.
+     * Generate a downsampled thumbnail if we don't already have one
      */
 
-    var MAX_STRIP_LENGTH = 64;
-    var THUMBNAIL_SCALE_LOG2 = 3;   // Log2 of the amount to downscale thumbnails by before storage
-    var NOISE_THRESHOLD = 10;       // Diffs less than this mean the image hasn't changed meaningfully
-    var MAXIMUM_GAP = 2;            // Maximum number of consecutive missing LEDs before we give up on a strip
+    if (jNode.thumbFile && fs.existsSync(path.join(io.dataPath, jNode.thumbFile))) {
+        return callback();
+    }
 
-    var strip = this;
-    this.device = device;
-    this.index = index;
-    this.stripLength = MAX_STRIP_LENGTH;
-    this.nextLed = 0;
-    this.thumbnails = [];
-    this.peakDiffs = [];
-    this.complete = false;
+    async.waterfall([
 
-    this.toString = function () {
-        return sprintf("fc %s strip %d", strip.device.serial, strip.index);
-    };
+        // Extract the thumbnail image (much faster than full processing)
+        async.apply(exec, 'dcraw -e -c ' + path.join(io.dataPath, jNode.rawFile),
+            {encoding: 'binary', maxBuffer: 100 * 1024 * 1024}),
 
-    this.step = function (stepNumber, fc, camera, callback) {
-        var thisLed = strip.nextLed;
-        if (thisLed >= this.stripLength) {
-            this.complete = true;
+        // Let OpenCV parse and decompress the image
+        function (stdout, stderr, callback) {
+            var jpeg = new Buffer(stdout, 'binary');
+            cv.readImage(jpeg, callback);
+        },
+
+        // Compute and save the thumbnail
+        function (img, callback) {
+            img.convertGrayscale();
+            for (var i = 0; i < THUMBNAIL_SCALE_LOG2; i++) {
+                img.pyrDown();
+            }
+
+            var thumbFile = 'thumb-' + name + '.png';
+            var filename = path.join(io.dataPath, thumbFile);
+            var tempFile = filename + tempSuffix();
+
+            img.save(tempFile);
+            fs.rename(tempFile, filename, function (err) {
+
+                // Success
+                jNode.thumbFile = thumbFile;
+                callback();
+
+            });
+        },
+    ], callback);
+}
+
+
+function photographCommon(name, io, jNode, prepFn, photoCallback, finalCallback)
+{
+    /*
+     * Core photography method shared for LED photos and dark frames.
+     * prepFn() is invoked only if the photo needs to be taken, prior to shooting.
+     * photoCallback() is invoked as soon as the camera is free,
+     * and finalCallback() when the photo and thumbnail are both available.
+     */
+
+    if (jNode.rawFile && fs.existsSync(path.join(io.dataPath, jNode.rawFile))) {
+        photoCallback();
+        finalCallback();
+        return;
+    }
+
+    prepFn(function (err) {
+        if (err) return photoCallback(err);
+
+        io.camera.takePicture({download: true}, function (err, image) {
+            if (err) return photoCallback(err);
+
+            // Timestamp as soon as the photo was taken
+            jNode.timestamp = new Date().toJSON();
+
+            // As soon as we've taken the picture, move on to the next step
+            // and process the photo a little in the background.
+            photoCallback();
+
+            // Asynchronously write RAW image to disk
+            var rawFile = 'raw-' + name + '.CR2';
+            atomicWriteFile(path.join(io.dataPath, rawFile), image, function(err) {
+                if (err) return finalCallback(err);
+                jNode.rawFile = rawFile;
+                generateThumbnail(name, io, jNode, finalCallback);
+            });
+        });
+    });
+}
+
+
+function photographLed(led, io, jLed, photoCallback, finalCallback)
+{
+    // Photograph a single LED only if the json doesn't already contain a valid RAW photo.
+
+    photographCommon(led.string, io, jLed, function (callback) {
+
+        console.log('Photographing ' + led.string);
+        io.fc.singleLight({serial: led.device}, led.index, callback);
+
+    }, photoCallback, finalCallback);
+}
+
+
+function photographDarkness(io, json, index, callback)
+{
+    /*
+     * Take a dark frame, store it with the indicated numerical index.
+     * Dark frames don't have the fancy pipelined callbacks that normal LED
+     * frames do, so it's easier to ensure the dark frame thumbnail is ready
+     * before it's referenced by any other frames.
+     */
+
+    var jFrame = (json.darkFrames[index] = json.darkFrames[index] || {});
+
+    photographCommon('dark-' + index, io, jFrame, function (callback) {
+
+        console.log('Photographing dark frame ' + index);
+        io.fc.lightsOff(callback);
+
+    }, function (err) {
+        // Photo done only; wait for the full completion on dark frames
+        if (err) return callback(err);
+    },
+    callback);
+}
+
+
+function currentDarkFrameIndex(json)
+{
+    /*
+     * Return a dark frame index to use for the current time.
+     * If the latest dark frame is still recent enough, returns its index.
+     * Otherwise returns the next unused index.
+     */
+
+    var index = json.darkFrames.length - 1;
+    var frame = json.darkFrames[index];
+
+    if (frame && frame.timestamp) {
+        var ts = new Date(frame.timestamp).getTime();
+        if (Date.now() - ts < DARK_INTERVAL * 1000) {
+            return index;
+        }
+    }
+
+    return index + 1;
+}
+
+
+function generatePeakDiff(io, json, jLed, callback)
+{
+    /*
+     * Calculate the peak difference between this LED's image and its dark
+     * frame, using only the tiny thumbnails. Store the result in 'peakDiff'.
+     * Calculation is skipped if it's already been done.
+     */
+
+    if (jLed.peakDiff != undefined) {
+        callback();
+    }
+
+    async.map([
+        path.join(io.dataPath, json.darkFrames[jLed.darkFrame].thumbFile),
+        path.join(io.dataPath, jLed.thumbFile),
+    ],
+    cv.readImage, function (err, img) {
+        if (err) return callback(err);
+
+        // Meh, the RGB channels seem to come back when saving/loading thumbnails
+        img[0].convertGrayscale();
+        img[1].convertGrayscale();
+
+        var diff = new cv.Matrix(img[0].width(), img[0].height());
+        diff.absDiff(img[0], img[1]);
+
+        jLed.peakDiff = diff.minMaxLoc().maxVal;
+        callback();
+    });
+}
+
+
+function updateStripLength(stripIndex, jDev, callback)
+{
+    /*
+     * If we don't know this strip's length already, look for
+     * runs of LEDs with a peakDiff below our threshold.
+     */
+
+    var jStrip = jDev.strips[stripIndex];
+
+    if (jStrip.length != undefined) {
+        // Already calculated
+        return callback();
+    }
+
+    var gap = null;
+    for (var i = 0; i < fadecandy.LEDS_PER_STRIP; i++) {
+        var ledIndex = i + stripIndex * fadecandy.LEDS_PER_STRIP;
+        var jLed = jDev.leds[ledIndex];
+
+        if (!jLed || jLed.peakDiff == undefined) {
+            // Not enough information yet
             return callback();
         }
 
-        var fcLedIndex = strip.index * MAX_STRIP_LENGTH + thisLed;
-        strip.nextLed += 1;
+        if (jLed.peakDiff >= NOISE_THRESHOLD) {
+            // LED is visible
+            gap = null;
+            continue;
+        }
 
-        console.log(sprintf("[%s] Step %d, photographing led %d", strip.toString(), stepNumber, thisLed));
+        // LED appears to be missing, gap begins or continues
+        if (gap == null) {
+            gap = i;
+        }
 
-        var filenameSlug = sprintf('%05d-%s-%03d', stepNumber, this.device.serial, fcLedIndex);
+        if (i - gap >= MAXIMUM_GAP) {
+            // Assume this strip has ended
+            jStrip.length = gap;
+            return callback();
+        }
+    }
 
-        fc.singleLight(strip.device, fcLedIndex, function (err) {
-            if (err) return callback(err);
+    // Full length strip
+    jStrip.length = fadecandy.LEDS_PER_STRIP;
+    return callback();
+}
 
-            camera.takePicture({download: true}, function (err, image) {
-                if (err) return callback(err);
 
-                // As soon as we've taken the picture, move on to the next step
-                // and process the photo a little in the background.
+function handleOneLed(led, io, json, photoCallback, finalCallback)
+{
+    /*
+     * Handle the photography and processing for a single LED.
+     * Skips any steps that have already been completed and recorded.
+     */
 
-                callback();
+    var jDev = (json.devices[led.device] = json.devices[led.device] || {strips: {}, leds: {}});
+    var jStrip = (jDev.strips[led.stripIndex] = jDev.strips[led.stripIndex] || {});
 
-                strip.processImage(image, filenameSlug, thisLed, function (err) {
-                    if (err) return callback(err);
-                });
+    // Skip LEDs that are beyond the detected strip length
+    if (jStrip.length != undefined && led.stripPosition >= jStrip.length) {
+        photoCallback();
+        finalCallback();
+        return;
+    }
+
+    var jLed = (jDev.leds[led.index] = jDev.leds[led.index] || {});
+    var darkFrame = jLed.darkFrame || currentDarkFrameIndex(json);
+
+    photographDarkness(io, json, darkFrame, function (err) {
+        if (err) return finalCallback(err);
+
+        photographLed(led, io, jLed, photoCallback, function (err) {
+            if (err) return finalCallback(err);
+
+            // Dark frame, taken in the recent past
+            jLed.darkFrame = darkFrame;
+
+            // Calculate the peak pixel difference between this photo and the dark frame
+            generatePeakDiff(io, json, jLed, function (err) {
+                if (err) return finalCallback(err);
+
+                // Update strip length if we can
+                updateStripLength(led.stripIndex, jDev, finalCallback);
             });
         });
-    };
-
-    this.processImage = function (image, filenameSlug, thisLed, callback) {
-        var rawFile = 'data/raw-' + filenameSlug + '.CR2';
-        var thumbFile = 'data/thumb-' + filenameSlug + '.jpg';
-
-        async.waterfall([
-
-            async.apply(fs.writeFile, rawFile, image),
-
-            // Extract the thumbnail image (much faster than full processing)
-            async.apply(exec, 'dcraw -e -c ' + rawFile, {encoding: 'binary', maxBuffer: 100 * 1024 * 1024}),
-
-            // Let OpenCV parse and decompress the image
-            function (stdout, stderr, callback) {
-                var jpeg = new Buffer(stdout, 'binary');
-                cv.readImage(jpeg, callback);
-            },
-
-            // Store a tiny grayscale version
-            function (img, callback) {
-                img.convertGrayscale();
-                for (var i = 0; i < THUMBNAIL_SCALE_LOG2; i++) {
-                    img.pyrDown();
-                }
-                strip.thumbnails[thisLed] = img;
-                img.save(thumbFile);
-                callback();
-            },
-
-            // Look at the string of thumbnails we have so far, to decide if the strip has ended
-            strip.analyzeThumbnails,
-
-        ], callback);
-    };
-
-    this.analyzeThumbnails = function (callback) {
-        var i = 0;
-        var img1, img2;
-
-        // Calculate any peaks we're missing that we have source data for.
-        while ( (img1 = strip.thumbnails[i])
-            &&  (img2 = strip.thumbnails[i + 1]) ) {
-            if (strip.peakDiffs[i] == undefined) {
-                strip.peakDiffs[i] = peakImageDiff(img1, img2);
-            }
-            i++;
-        }
-
-        if (strip.peakDiffs.length > 0) {
-            console.log(sprintf("[%s] diffs: %s", strip.toString(), strip.peakDiffs));
-        }
-
-        // Have we found the end of the strip?
-        var beginGap = null;
-        for (var i = 0; i < strip.peakDiffs.length; i++) {
-            if (strip.peakDiffs[i] < NOISE_THRESHOLD) {
-                // Images i and i+1 seem like they're the same.
-                // This means neither image has a visible LED, so the gap begins or continues.
-                if (beginGap == null) {
-                    beginGap = i;
-                }
-
-                if (i - beginGap >= MAXIMUM_GAP) {
-                    // Assume this strip has ended
-                    strip.setLength(beginGap);
-                    break;
-                }
-            } else {
-                // Not a gap
-                beginGap = null;
-            }
-        }
-
-        callback();
-    };
-
-    this.setLength = function (length) {
-        console.log(sprintf("[%s] determined length: %d", strip.toString(), length));
-        strip.stripLength = length;
-    };
+    });
 }
 
 
@@ -197,70 +338,72 @@ function cameraSetup(callback)
 }
 
 
-function main(callback)
+function photographer(dataPath, callback)
 {
+    if (!fs.statSync(dataPath).isDirectory()) {
+        return callback("Data path must be a directory. Create a new empty directory to start from scratch.");
+    }
+
+    var jsonPath = path.join(dataPath, 'photos.json');
+    var json = JSON.parse(fs.existsSync(jsonPath) ? fs.readFileSync(jsonPath) : '{}');
+    json.devices = json.devices || {};
+    json.darkFrames = json.darkFrames || [];
+
+    // Persistent random seed
+    if (json.random) {
+        randy.setState(json.random);
+    }
+    json.random = randy.getState();
+
     async.parallel({
 
         fc: async.apply(fadecandy.connect, 'ws://localhost:7890'),
         camera: cameraSetup,
 
-    }, function (err, data) {
+    }, function (err, io) {
 
         if (err) return callback(err);
-        var fc = data.fc;
-        var camera = data.camera;
+        io.dataPath = dataPath;
 
-        // We know about all of the attached Fadecandy boards; now create LEDStrip instances
+        // Shuffle the list of possible LEDs, so we visit them in an order that's been
+        // decorrelated from their physical position.
 
-        var strips = [];
-        for (var dev = 0; dev < fc.devices.length; dev++) {
-            for (var idx = 0; idx < 8; idx++) {
-                strips.push(new LEDStrip(fc.devices[dev], idx));
-            }
-        }
+        var leds = fadecandy.enumerateDeviceLeds(io.fc.devices);
+        randy.shuffleInplace(leds);
+        leds.sort(function (a,b) { return a.stripPosition - b.stripPosition; });
 
-        // Keep another array with just the strips we know still need work
-        var pendingStrips = strips.slice();
-        var stepNumber = 0;
-        var seed = '';
+        // Photograph each LED, saving our state after each one
 
-        var nextStep = function (err) {
+        async.mapSeries(leds, function (led, callback) {
+
+            handleOneLed(led, io, json, callback, function (err) {
+                // Asynchronous completion callback
+                if (err) return callback(err);
+                atomicWriteFile(jsonPath, JSON.stringify(json, null, '\t'), function (err) {
+                    if (err) return callback(err);
+                });
+            });
+
+        }, function (err) {
             if (err) return callback(err);
 
-            stepNumber += 1;
-            var shasum = crypto.createHash('sha1');
-            shasum.update(seed + stepNumber);
-            var stepHash = shasum.digest();
-
-            // Done? Turn off the lights and close the FC connection
-            if (pendingStrips.length == 0) {
-                fc.lightsOff(function (err) {
-                    if (err) return callback(err);
-                    fc.socket.close();
-                    callback();
-                });
-                return;
-            }
-
-            // Repeatably choose a random pending strip
-            var stripArrayIndex = stepHash.readUInt32LE(2) % pendingStrips.length;
-            var strip = pendingStrips[stripArrayIndex];
-
-            strip.step(stepNumber, fc, camera, function (err) {
+            console.log("Done!");
+            io.fc.lightsOff(function (err) {
                 if (err) return callback(err);
-                if (strip.complete) {
-                    pendingStrips.splice(stripArrayIndex, 1);
-                }
-                nextStep();
+                io.fc.socket.close();
+                callback();
             });
-        }
-
-        nextStep();
+        });
     });
 }
 
 
-main(function (err) {
+if (process.argv.length != 3) {
+    console.log("usage: photographer.js <data path>");
+    process.exit(1);
+}
+
+photographer(process.argv[2], function (err) {
     if (err) {
         console.log(err);
         process.exit(1);
